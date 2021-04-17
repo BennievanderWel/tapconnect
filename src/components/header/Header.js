008f11b530a3e426bc5c7dceeaecfd1f8b692391
Bennie van der Wel <bennievanderwel@gmail.com>
2021-04-17 12:11:14 +0200

    Header bug fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,12 +5,16 @@ import { useIsSmallScreen } from '../utils';
 import styles from './Header.module.scss';
 
 const Header = ({ selectedChat, toggleSidebar, isSidebarOpen }) => {
+  const isSmallScreen = useIsSmallScreen();
+
   return (
     <div className={styles.container}>
-      {useIsSmallScreen() && (
+      {isSmallScreen && (
         <Button isRounded icon="menu" onClick={toggleSidebar} />
       )}
-      <h1>{isSidebarOpen ? 'TapConnect' : selectedChat.name}</h1>
+      <h1>
+        {isSidebarOpen && isSmallScreen ? 'TapConnect' : selectedChat.name}
+      </h1>
     </div>
   );
 };
